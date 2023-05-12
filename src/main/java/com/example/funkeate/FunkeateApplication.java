b05d7ae34ff6b7bd4767cc6fca779d2b8d56b910
Jacko <jacko.tinoco@tecsup.edu.pe>
2023-05-12 18:39:06 -0500

    First Commit

---
+++ src/main/java/com/example/funkeate/FunkeateApplication.java
@@ -0,0 +1,13 @@
+package com.example.funkeate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunkeateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunkeateApplication.class, args);
+	}
+
+}
